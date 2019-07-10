fbad4a042d355eda0c972540374b41fc25323592
Khalotis11 <moekhaloti@gmail.com>
2019-07-11 00:41:27 +0100

    Added action type and action creator

--- todolist/src/reducer/index.js
+++ todolist/src/reducer/index.js
@@ -1,3 +1,15 @@
-export const reducer = state => {
-  return state;
+import { ADD_TODO } from "../actions";
+
+const initialState = {
+  todos: "Do Homework"
+};
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return {
+        ...state
+      };
+    default:
+      return state;
+  }
 };
